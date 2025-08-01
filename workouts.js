4f0731558967441b67a5b7489974cac7db820a9c
BlackMonkey0 <127506167+BlackMonkey0@users.noreply.github.com>
2025-08-01 14:18:27 +0200

    Update workouts.js

--- workouts.js
+++ workouts.js
@@ -1,14 +1,219 @@
-window.RUTINAS = RUTINAS;
-const RUTINAS = {
+window.RUTINAS = {
   "2025-08-01": [
-    { nombre: "Flexiones", descripcion: "3 series de 12 repeticiones" },
-    { nombre: "Sentadillas", descripcion: "3 series de 15 repeticiones" },
-    { nombre: "Plancha", descripcion: "3 series de 30 segundos" }
+    { nombre: "Calentamiento", descripcion: "Saltar a la cuerda o trotar en el sitio - 5 min" },
+    { nombre: "Sentadillas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Zancadas alternas", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Puente de glúteos", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos de estiramiento general" }
   ],
   "2025-08-02": [
-    { nombre: "Fondos en silla", descripcion: "3 series de 10 repeticiones" },
-    { nombre: "Zancadas", descripcion: "3 series por pierna" },
-    { nombre: "Elevaciones de piernas", descripcion: "3 series de 15 repeticiones" }
+    { nombre: "Calentamiento", descripcion: "Caminata rápida en el lugar - 5 min" },
+    { nombre: "Flexiones", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 10 repeticiones" },
+    { nombre: "Press militar con botellas", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
   ],
-  // Agrega más días aquí
+  "2025-08-03": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + trotar en el sitio - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Plancha abdominal", descripcion: "3 series de 45 segundos" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-04": [
+    { nombre: "Calentamiento", descripcion: "Saltar en el sitio - 5 min" },
+    { nombre: "Flexiones inclinadas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Dominadas australianas (bajo mesa)", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha lateral", descripcion: "3 series x 30 segundos por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-05": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho en el sitio - 5 min" },
+    { nombre: "Sentadillas", descripcion: "4 series x 15 repeticiones" },
+    { nombre: "Zancadas", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Hip Thrust (elevación de cadera)", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-06": [
+    { nombre: "Calentamiento", descripcion: "Trote suave en el sitio - 5 min" },
+    { nombre: "Flexiones", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 10 repeticiones" },
+    { nombre: "Press militar con botellas", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-07": [
+    { nombre: "Calentamiento", descripcion: "Saltar a la cuerda o simular cuerda - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Plancha abdominal", descripcion: "3 series de 45 segundos" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-08": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos laterales - 5 min" },
+    { nombre: "Flexiones inclinadas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Dominadas australianas", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha lateral", descripcion: "3 series x 30 segundos por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-09": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Sentadillas", descripcion: "4 series x 15 repeticiones" },
+    { nombre: "Zancadas", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Hip Thrust", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-10": [
+    { nombre: "Calentamiento", descripcion: "Trote suave - 5 min" },
+    { nombre: "Flexiones", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 10 repeticiones" },
+    { nombre: "Press militar con botellas", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+ "2025-08-11": [
+    { nombre: "Calentamiento", descripcion: "Saltar en el sitio - 5 min" },
+    { nombre: "Sentadillas con salto", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Zancadas hacia atrás", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Plancha con toque de hombro", descripcion: "3 series x 20 toques" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-12": [
+    { nombre: "Calentamiento", descripcion: "Caminata rápida en el sitio - 5 min" },
+    { nombre: "Flexiones abiertas", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Plancha lateral con elevación de pierna", descripcion: "3 series x 12 repeticiones por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-13": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos suaves - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Abdominales bicicleta", descripcion: "3 series x 20 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-14": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Flexiones declinadas (pies en silla)", descripcion: "4 series x 8 repeticiones" },
+    { nombre: "Dominadas australianas (debajo de mesa)", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha isométrica", descripcion: "3 series de 45 segundos" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-15": [
+    { nombre: "Calentamiento", descripcion: "Trote suave - 5 min" },
+    { nombre: "Sentadillas profundas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Zancadas laterales", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Hip Thrust", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-16": [
+    { nombre: "Calentamiento", descripcion: "Saltar en el sitio - 5 min" },
+    { nombre: "Flexiones diamante", descripcion: "4 series x 8 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Press militar con botellas", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-17": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos suaves - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Plancha abdominal", descripcion: "3 series de 1 min" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-18": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Flexiones inclinadas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Dominadas australianas", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha lateral", descripcion: "3 series x 30 segundos por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-19": [
+    { nombre: "Calentamiento", descripcion: "Saltar a la cuerda o simular cuerda - 5 min" },
+    { nombre: "Sentadillas con salto", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Zancadas hacia atrás", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Plancha con toque de hombro", descripcion: "3 series x 20 toques" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-20": [
+    { nombre: "Calentamiento", descripcion: "Caminata rápida en el sitio - 5 min" },
+    { nombre: "Flexiones abiertas", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Plancha lateral con elevación de pierna", descripcion: "3 series x 12 repeticiones por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-21": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos suaves - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Abdominales bicicleta", descripcion: "3 series x 20 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+"2025-08-22": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Flexiones declinadas (pies en silla)", descripcion: "4 series x 8 repeticiones" },
+    { nombre: "Dominadas australianas (debajo de mesa)", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha isométrica", descripcion: "3 series de 45 segundos" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-23": [
+    { nombre: "Calentamiento", descripcion: "Trote suave - 5 min" },
+    { nombre: "Sentadillas profundas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Zancadas laterales", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Hip Thrust", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-24": [
+    { nombre: "Calentamiento", descripcion: "Saltar en el sitio - 5 min" },
+    { nombre: "Flexiones diamante", descripcion: "4 series x 8 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Press militar con botellas", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-25": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos suaves - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Plancha abdominal", descripcion: "3 series de 1 min" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-26": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Flexiones inclinadas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Dominadas australianas", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha lateral", descripcion: "3 series x 30 segundos por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-27": [
+    { nombre: "Calentamiento", descripcion: "Saltar a la cuerda o simular cuerda - 5 min" },
+    { nombre: "Sentadillas con salto", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Zancadas hacia atrás", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Plancha con toque de hombro", descripcion: "3 series x 20 toques" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-28": [
+    { nombre: "Calentamiento", descripcion: "Caminata rápida en el sitio - 5 min" },
+    { nombre: "Flexiones abiertas", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Fondos en silla", descripcion: "3 series x 12 repeticiones" },
+    { nombre: "Plancha lateral con elevación de pierna", descripcion: "3 series x 12 repeticiones por lado" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-29": [
+    { nombre: "Calentamiento", descripcion: "Movilidad articular + saltos suaves - 5 min" },
+    { nombre: "Sentadilla búlgara", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Peso muerto con mochila", descripcion: "4 series x 10 repeticiones" },
+    { nombre: "Abdominales bicicleta", descripcion: "3 series x 20 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-30": [
+    { nombre: "Calentamiento", descripcion: "Rodillas al pecho - 5 min" },
+    { nombre: "Flexiones declinadas (pies en silla)", descripcion: "4 series x 8 repeticiones" },
+    { nombre: "Dominadas australianas (debajo de mesa)", descripcion: "3 series x 8 repeticiones" },
+    { nombre: "Plancha isométrica", descripcion: "3 series de 45 segundos" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ],
+  "2025-08-31": [
+    { nombre: "Calentamiento", descripcion: "Trote suave - 5 min" },
+    { nombre: "Sentadillas profundas", descripcion: "4 series x 12 repeticiones" },
+    { nombre: "Zancadas laterales", descripcion: "3 series x 12 repeticiones por pierna" },
+    { nombre: "Hip Thrust", descripcion: "3 series x 15 repeticiones" },
+    { nombre: "Estiramientos", descripcion: "5 minutos" }
+  ]
 };
